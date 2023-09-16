4b99b3a6ef8444b2d9072714d79acbe92aaef4a0
João Henrique <joaohnsousa@gmail.com>
2023-09-16 20:51:37 -0300

    Controle de fluxo

---
+++ controle-de-fluxo/Contador/src/ParametrosInvalidosException.java
@@ -0,0 +1,11 @@
+public class ParametrosInvalidosException extends Exception {
+    public static void main(String[] args) {
+       
+    }
+
+    public static void mostrarError(){
+        
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        
+       }
+}
