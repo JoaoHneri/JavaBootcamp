baef7a691edabe96d01d16d59574ed2bb5535bf7
João Henrique <joaohnsousa@gmail.com>
2023-09-23 11:07:35 -0300

    Spring Data JPA

---
+++ aula-spring-data-jpa/src/main/java/dio/aula/model/User.java
@@ -0,0 +1,53 @@
+package dio.aula.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tab_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer id;
+    @Column(length = 50, nullable = false)
+    private String name;
+    @Column(length = 20, nullable = false)
+    private String Username;    
+    @Column(length = 100, nullable = false)
+    private String Password;
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getUsername() {
+        return Username;
+    }
+    public void setUsername(String username) {
+        Username = username;
+    }
+    public String getPassword() {
+        return Password;
+    }
+    public void setPassword(String password) {
+        Password = password;
+    }
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + ", Username=" + Username + ", Password=" + Password + "]";
+    }
+
+    
+}
